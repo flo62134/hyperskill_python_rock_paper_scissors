708018ede9f994f109fc28ad5a61c5de63036551
flo62134 <florentbrassart31@gmail.com>
2020-05-23 19:17:43 +0200

    Completed stage 3

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,6 +1,12 @@
 # Write your code here
 import random
 
+INVALID_INPUT = 'Invalid input'
+
+END = 'Bye!'
+
+EXIT = '!exit'
+
 ROCK = 'rock'
 PAPER = 'paper'
 SCISSORS = 'scissors'
@@ -11,17 +17,17 @@ DRAW = 'There is a draw ({0})'
 WIN = 'Well done. Computer chose {0} and failed'
 
 
-def get_winning_play(play: str):
-    if play == ROCK:
+def get_computer_winning_play(user_play: str):
+    if user_play == ROCK:
         return PAPER
-    elif play == PAPER:
+    elif user_play == PAPER:
         return SCISSORS
     else:
         return ROCK
 
 
 def get_round_state(user_play: str, computer_play: str):
-    winning_play = get_winning_play(user_play)
+    winning_play = get_computer_winning_play(user_play)
 
     if user_play == computer_play:
         return DRAW
@@ -33,7 +39,24 @@ def format_round_state(state: str, computer_play: str):
     return state.format(computer_play)
 
 
-user_play = input()
-computer_play = random.choice(PLAYS)
-round_state = get_round_state(user_play, computer_play)
-print(format_round_state(round_state, computer_play))
+def play_round(user_play: str):
+    computer_play = random.choice(PLAYS)
+    round_state = get_round_state(user_play, computer_play)
+    print(format_round_state(round_state, computer_play))
+
+
+def is_valid_play(play: str):
+    return play in PLAYS
+
+
+user_play = ''
+while user_play != EXIT:
+    user_play = input()
+    is_valid = is_valid_play(user_play)
+    if not is_valid:
+        print(INVALID_INPUT)
+        continue
+
+    play_round(user_play)
+
+print(END)
