fbda3e42a195549824ed0a5588eb448885d5f403
flo62134 <florentbrassart31@gmail.com>
2020-05-23 18:42:13 +0200

    Completed stage 1

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,1 +1,18 @@
 # Write your code here
+ROCK = 'rock'
+PAPER = 'paper'
+SCISSORS = 'scissors'
+
+
+def get_winning_play(play: str):
+    if play == ROCK:
+        return PAPER
+    elif play == PAPER:
+        return SCISSORS
+    else:
+        return ROCK
+
+
+user_play = input()
+winning_play = get_winning_play(user_play)
+print(f"Sorry, but computer chose {winning_play}")
